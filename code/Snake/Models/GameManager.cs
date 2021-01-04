1fe427a75cd7fd44bb701c4daa3a330434ff65b1
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2021-01-04 15:17:48 +0300

    Copy code

---
+++ code/Snake/Models/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Snake.Models
+{
+    public class GameManager : IGameManager
+    {
+        private ConcurrentDictionary<Guid, GameBoard> _gameBoards = new ConcurrentDictionary<Guid, GameBoard>();
+                
+        /// <summary>
+        /// Создает новую игру с заданными параметрами
+        /// </summary>
+        /// <param name="boardSize">Размер поля</param>
+        /// <param name="turnTimeInMilliseconds">Время одного хода в миллисекундах</param>
+        public Guid CreateNewGameBoard(Size boardSize, int turnTimeInMilliseconds)
+        {
+            GameBoard gameboard = new GameBoard(boardSize, turnTimeInMilliseconds);
+            Guid id = Guid.NewGuid();
+            while (!_gameBoards.TryAdd(id, gameboard))
+                id = Guid.NewGuid();
+            return id;
+        }
+        
+        /// <summary>
+        /// Возвращает игру по id
+        /// </summary>
+        public IGameBoard GetGameBoard(Guid id)
+        {
+            GameBoard _gameBoard;
+            _gameBoards.TryGetValue(id, out _gameBoard);
+            if (_gameBoard != null)
+                _gameBoard.CountTimeUntilNextTurnMiliseconds();
+            return _gameBoard;
+        }
+    }
+}
---
+++ code/Snake/Models/TimerManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Snake.Models
+{
+    /// <summary>
+    /// Отвечает за запуск функции по таймеру и 
+    /// взаимодействие со временем связанным с таймером
+    /// </summary>
+    public class TimerManager
+    {
+        private TimeSpan _turnTime;
+        private Timer _onUpdateTimer;        
+        private Stopwatch _stopwatch;
+        
+        /// <summary>
+        /// Менеджер для периодично запускаемой function
+        /// </summary>
+        /// <param name="turnTimeInMilliseconds">Периодичность запуска</param>
+        /// <param name="function">Периодично запускаема функция</param>
+        public TimerManager(int turnTimeInMilliseconds, TimerCallback function)
+        {
+            _turnTime = new TimeSpan(0, 0, 0, 0, turnTimeInMilliseconds);
+            _onUpdateTimer = new Timer(function);
+            _stopwatch = new Stopwatch();
+        }
+        
+        /// <summary>
+        /// Запуск таймера
+        /// </summary>
+        public void StartTimer()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            _onUpdateTimer.Change(0, (int)_turnTime.TotalMilliseconds); // запуск периодичного function
+        }
+
+        /// <summary>
+        /// Перезапуск часов
+        /// </summary>
+        public void ResetStopwatch()
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+        }
+
+        /// <summary>
+        /// Возвращает время до наступления следующего хода
+        /// </summary>
+        public int CountTimeUntilNextTurnMiliseconds()
+        {
+            TimeSpan remainingTime = _turnTime.Subtract(_stopwatch.Elapsed);
+            return (int)remainingTime.TotalMilliseconds;
+        }
+    }
+}
